3ccdd240bdc73771eb16bdefde5e629c02738202
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-08-23 12:58:21 +0900

    Add textview

--- iOSSampleProject/ViewController.swift
+++ iOSSampleProject/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let textView = UITextView(frame: CGRect(x: 20.0, y: 90.0, width: 250.0, height: 100.0))
+        
+        textView.center = self.view.center
+        textView.textAlignment = NSTextAlignment.justified
+        textView.textColor = UIColor.blue
+        textView.backgroundColor = UIColor.lightGray
+        textView.text = "It is textview"
+
+        self.view.addSubview(textView)
     }
 
 
