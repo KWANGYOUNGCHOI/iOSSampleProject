ec15ccbd4e7caa415b28521c757ac94d1f90fe13
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-08-23 14:16:55 +0900

    Add textview and constraint programmatically

--- iOSSampleProject/ViewController.swift
+++ iOSSampleProject/ViewController.swift
@@ -13,15 +13,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let textView = UITextView(frame: CGRect(x: 20.0, y: 90.0, width: 250.0, height: 100.0))
+//        let textView = UITextView(frame: CGRect(x: 20.0, y: 90.0, width: 250.0, height: 100.0))
+//
+//        textView.center = self.view.center
+//        textView.textAlignment = NSTextAlignment.justified
+//        textView.textColor = UIColor.blue
+//        textView.backgroundColor = UIColor.lightGray
+//        textView.text = "It is textview"
+//
+//        self.view.addSubview(textView)
         
-        textView.center = self.view.center
-        textView.textAlignment = NSTextAlignment.justified
+        let textView = UITextView()
+        self.view.addSubview(textView)
+
+        textView.translatesAutoresizingMaskIntoConstraints = false
+
+        var constraints = [NSLayoutConstraint]()
+
+        let constraintTop = NSLayoutConstraint(item: textView,
+            attribute: NSLayoutConstraint.Attribute.top,
+            relatedBy: NSLayoutConstraint.Relation.equal,
+            toItem: self.view.safeAreaLayoutGuide,
+            attribute: NSLayoutConstraint.Attribute.top,
+            multiplier: 1.0,
+            constant: 10)
+        constraints.append(constraintTop)
+
+        let constraintBottom = NSLayoutConstraint(item: textView,
+            attribute: NSLayoutConstraint.Attribute.bottom,
+            relatedBy: NSLayoutConstraint.Relation.equal,
+            toItem: self.view.safeAreaLayoutGuide,
+            attribute: NSLayoutConstraint.Attribute.bottom,
+            multiplier: 1.0,
+            constant: -10)
+        constraints.append(constraintBottom)
+
+        let constraintLeft = NSLayoutConstraint(item: textView,
+            attribute: NSLayoutConstraint.Attribute.leading,
+            relatedBy: NSLayoutConstraint.Relation.equal,
+            toItem: self.view.safeAreaLayoutGuide,
+            attribute: NSLayoutConstraint.Attribute.leading,
+            multiplier: 1.0,
+            constant: 10)
+        constraints.append(constraintLeft)
+
+        let constraintRight = NSLayoutConstraint(item: textView,
+            attribute: NSLayoutConstraint.Attribute.trailing,
+            relatedBy: NSLayoutConstraint.Relation.equal,
+            toItem: self.view.safeAreaLayoutGuide,
+            attribute: NSLayoutConstraint.Attribute.trailing,
+            multiplier: 1.0,
+            constant: -10)
+        constraints.append(constraintRight)
+
+        self.view.addConstraints(constraints)
+        
         textView.textColor = UIColor.blue
         textView.backgroundColor = UIColor.lightGray
-        textView.text = "It is textview"
-
-        self.view.addSubview(textView)
+        textView.text = "It is TextView"
     }
 
 }
